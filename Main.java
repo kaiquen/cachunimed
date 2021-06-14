5f410da866578c4ebe98a0e080b1653f663c2545
Kaique N <kaiquenascente@gmail.com>
2021-06-14 17:11:57 -0300

    add /View e /Controlle

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        
+        Parent login = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
+        Scene scene = new Scene(login);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+
     }
     public static void main(String[] args) {
         launch(args);
---
+++ controller/LoginController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class LoginController implements Initializable {
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
