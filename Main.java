5cf6ebcb438f6a6e045d0ae0aec4a25aaaf68072
Kaique N <kaiquenascente@gmail.com>
2021-06-14 16:38:36 -0300

    add project structure

---
+++ Main.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
