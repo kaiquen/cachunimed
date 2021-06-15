7317cb377ef59d12af16235d6880ee2cb9e585f3
Kaique N <kaiquenascente@gmail.com>
2021-06-15 13:56:50 -0300

    add verify combobox, textfield, cpf

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +14,11 @@ public class Main extends Application {
         stage.setTitle("Login");
         stage.setResizable(false);
         stage.setScene(scene);
+        
+        //**********
+        Image image = new Image("view/image/logo.jpg");
+        stage.getIcons().add(image);
+        
         stage.show();
     }
     
--- controller/diretor/CreateFuncionarioController.java
+++ controller/diretor/CreateFuncionarioController.java
@@ -10,21 +10,46 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import model.Types;
 
 public class CreateFuncionarioController implements Initializable{
 
 
     @FXML ComboBox<Types> comboBox;
-    
- 
+    @FXML TextField textFieldCpf;
+    @FXML TextField textFieldName;
+    @FXML PasswordField passwordField;
+
+    @FXML private void create(){
+        System.out.println(verifyField());
+    } 
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {  
         printTypes();
-
     }
 
-    public void printTypes(){
+    private boolean verifyField(){
+        String name =  textFieldName.getText();
+        String password = passwordField.getText();
+        boolean verifyField = (!name.isEmpty() && !password.isEmpty() && verifyCpf() && verifyComboBox()) ? true : false;
+        return verifyField;
+    }
+
+    private Boolean verifyCpf(){
+        String cpf = textFieldCpf.getText();
+        boolean verifyCPF = (!cpf.isEmpty() && cpf.length() == 11 && cpf.matches("[0-9]+")) ? true : false;
+        return verifyCPF;
+    }
+
+    private Boolean verifyComboBox(){
+        boolean verifyComboBox = !(comboBox.getValue().toString().isEmpty()) ? true : false;
+        return verifyComboBox;
+    }
+
+    private void printTypes(){
         List<Types> type = new ArrayList<>();
         ObservableList<Types> types;
     
