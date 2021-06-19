a86f5b2c20027b20c27a46b13572bfdb892bfb2a
Kaique N <kaiquenascente@gmail.com>
2021-06-19 15:29:13 -0300

    ok

--- model/dao/PacienteDAO.java
+++ model/dao/PacienteDAO.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
-import model.Funcionario;
 import model.Paciente;
 
 public class PacienteDAO {
